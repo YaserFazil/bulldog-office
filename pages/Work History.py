922aec3527306240e7c472a8783d8fd71d486639
YaserFazil <frfvipf@gmail.com>
2025-03-05 19:34:14 +0430

    apply function decimal to hhmm

--- pages/Work History.py
+++ pages/Work History.py
@@ -55,7 +55,7 @@ def main_work():
                             holiday_hours_value = st.session_state["edited_work_history_data"]["Hours Holiday"][0] if "Hours Holiday" in st.session_state["edited_work_history_data"] else "00:00"
                             holiday_hours_decimal = hhmm_to_decimal(holiday_hours_value)
                             holiday_hours_value = holiday_hours_decimal + diff
-                            holiday_hours = f"{int(holiday_hours_value)}:{int((holiday_hours_value - int(holiday_hours_value)) * 60):02d}"
+                            holiday_hours = decimal_hours_to_hhmmss(holiday_hours_value)
                         # elif work_time_decimal < standard_time_decimal and st.session_state.get("prev_holiday_hour") is None:
                         #     diff = standard_time_decimal
                         #     holiday_hours_value = st.session_state["edited_work_history_data"]["Hours Holiday"][0] if "Hours Holiday" in st.session_state["edited_work_history_data"] else "00:00"
